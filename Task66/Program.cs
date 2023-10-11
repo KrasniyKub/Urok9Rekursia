1a7d99f01ed859fb1d727654c3f17a6dfcafe90d
Dmitriy Sereda <nadechke@gmail.com>
2023-10-11 14:18:57 +0300

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+
+void PrintFactorial(int N, int M)
+{
+    Console.WriteLine("Сумма натуральных чисел от M до N:");
+    Console.WriteLine(PrintNm(N,M));
+}
+
+int PrintNm (int N, int M)
+{
+    int sum = 0;
+    while(N >= M)
+    {
+        sum = sum + N;
+        N--;
+    }
+    return sum;
+}
+
+int N = 12;
+
+int M = 4;
+
+PrintFactorial(N, M);
