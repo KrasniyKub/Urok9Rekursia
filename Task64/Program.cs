addcf8ac3c994954c8b82aa80462af6ddcb8e431
Dmitriy Sereda <nadechke@gmail.com>
2023-10-11 14:18:41 +0300

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.Clear();
+
+void PrintNfactorial(int N)
+{
+    Console.WriteLine("Натуральные числа от 1 до N:");
+    PrintN(N);
+}
+
+void PrintN (int N)
+{
+    Console.Write(N+" ");
+    N--;
+    if (N >= 1)
+    {
+        PrintN(N);
+    }
+    else
+    {
+        Console.WriteLine();
+    }
+}
+
+int N = 15;
+PrintNfactorial(N);
